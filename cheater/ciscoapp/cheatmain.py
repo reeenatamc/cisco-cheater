258263c55410f3969a8a6aadb9b82942ec3b884e
renataa <116390061+reeenatamc@users.noreply.github.com>
2025-05-24 20:54:34 -0500

    💄 new features!

--- cheater/ciscoapp/cheatmain.py
+++ cheater/ciscoapp/cheatmain.py
@@ -4,7 +4,6 @@ import os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-# Ruta absoluta al archivo
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Esto apunta a ciscoapp/
 json_path = os.path.join(BASE_DIR, 'diccionario.json')
 
@@ -16,7 +15,17 @@ with open(json_path, encoding='utf-8') as f:
 def buscar(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        pregunta = data.get('pregunta', '').strip()
-        respuesta = DICCIONARIO.get(pregunta, "❌ Pregunta no encontrada.")
+        pregunta = data.get('pregunta', '').strip().lower()
+        if pregunta in (key.lower() for key in DICCIONARIO):
+            clave_exacta = next(key for key in DICCIONARIO if key.lower() == pregunta)
+            respuesta = DICCIONARIO[clave_exacta]
+        else:
+            coincidencias = [DICCIONARIO[key] for key in DICCIONARIO if pregunta in key.lower()]
+            if coincidencias:
+                respuesta = coincidencias[0]
+            else:
+                respuesta = "❌ Pregunta no encontrada."
+
         return JsonResponse({'respuesta': respuesta})
+
     return JsonResponse({'error': 'Método no permitido'}, status=405)
