195b3aa6ccdd025ade5624e672f2d0fdb8d6dfe3
renataa <116390061+reeenatamc@users.noreply.github.com>
2025-10-19 12:58:31 -0500

    permitir todo

--- cheater/settings.py
+++ cheater/settings.py
@@ -357,10 +357,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # -------------------------
 # Configuración CORS
 # -------------------------
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    "https://*.railway.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://*.railway.app",
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]
